b6a28d47c59f0b5105cf31412f1e0aadc5dbf38b
Hiroshi Kumekawa <hkumekawa@gmail.com>
2017-05-30 08:28:33 +0900

    SpreadSheetからデータを読んでくるところまでワンパス通した。

--- handler.js
+++ handler.js
@@ -14,3 +14,53 @@ module.exports.hello = (event, context, callback) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+
+/**
+ * シートからデータを読んでくる
+ */
+module.exports.readSheet = (event, context, callback) => {
+  var GoogleSpreadsheet = require('google-spreadsheet');
+ 
+  var my_sheet = new GoogleSpreadsheet('107X7lFClUeHGija6Wl_rKRMUvbXGgvozQ1a2Na-RttA');
+  var credentials = require('./My_Project-29324953f4b4.json');
+
+
+  var sheet;
+  Promise.resolve(0).then((d)=>{
+
+    return new Promise((resolve, reject)=> {
+      my_sheet.useServiceAccountAuth(credentials, function(err){
+        if(err) {
+          reject(err)
+        }
+        else {
+          my_sheet.getInfo(function(err, data){
+            if(err) {
+              reject(err)
+            }
+            else {
+
+            }
+            resolve(data)
+          });
+        }
+      })
+    })
+  })
+  .then((data)=>{
+    sheet = data
+
+    for(var i in sheet.worksheets) {
+      if(sheet.worksheets[i].title === 'mysheet1') {
+        sheet.worksheets[i].getRows( function( err, rows ) {
+          for(var i in rows) {
+            console.log(rows[i]);
+          }
+        });
+      }
+    }  
+  })
+
+  callback(null, 'hello world');
+}
